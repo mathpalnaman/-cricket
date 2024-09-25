b8c220f25b5042dfb17b942602e322298c1a3ced
mathpalnaman <112200396+mathpalnaman@users.noreply.github.com>
2024-09-25 23:45:12 +0530

    vk

--- src/components/Steps/Step1.jsx
+++ src/components/Steps/Step1.jsx
@@ -1,87 +1,96 @@
 import React from "react";
 
-export default function Step1({ formData, handleChange, nextStep}) {
+export default function Step1({ formData, handleChange, nextStep }) {
   return (
-    <div>
-      <div className="mb-4">
-        <label className="block text-2xl font-semibold mb-1">Tournament Name:</label>
-        <input
-          type="text"
-          name="tournamentName"
-          value={formData.tournamentName}
-          onChange={handleChange}
-          className="w-full border rounded p-2"
-          placeholder="Enter the tournament name"
-          required
-        />
+    <div className="flex justify-between items-center h-screen">
+     
+      <div className="w-1/2 ml-10"> 
+        <div className="mb-4">
+          <label className="block text-sm font-medium mb-1">Tournament Name:</label>
+          <input
+            type="text"
+            name="tournamentName"
+            value={formData.tournamentName}
+            onChange={handleChange}
+            className="w-full border rounded p-2"
+            placeholder="Enter the tournament name"
+            required
+          />
+        </div>
+
+        <div className="mb-4">
+          <label className="block text-sm font-medium mb-1">City:</label>
+          <input
+            type="text"
+            name="city"
+            value={formData.city}
+            onChange={handleChange}
+            className="w-full border rounded p-2"
+            placeholder="Enter the city"
+            required
+          />
+        </div>
+
+        <div className="mb-4">
+          <label className="block text-sm font-medium mb-1">Stadium:</label>
+          <input
+            type="text"
+            name="stadium"
+            value={formData.stadium}
+            onChange={handleChange}
+            className="w-full border rounded p-2"
+            placeholder="Enter the stadium"
+            required
+          />
+        </div>
+
+        <div className="mb-4">
+          <label className="block text-sm font-medium mb-1">Organizer Name:</label>
+          <input
+            type="text"
+            name="organizerName"
+            value={formData.organizerName}
+            onChange={handleChange}
+            className="w-full border rounded p-2"
+            placeholder="Enter the organizer's name"
+            required
+          />
+        </div>
+
+        <div className="mb-4">
+          <label className="block text-sm font-medium mb-1">Start Date:</label>
+          <input
+            type="date"
+            name="startDate"
+            value={formData.startDate}
+            onChange={handleChange}
+            className="w-full border rounded p-2"
+            required
+          />
+        </div>
+
+        <div className="mb-4">
+          <label className="block text-sm font-medium mb-1">End Date:</label>
+          <input
+            type="date"
+            name="endDate"
+            value={formData.endDate}
+            onChange={handleChange}
+            className="w-full border rounded p-2"
+            required
+          />
+        </div>
+
+        <button
+          onClick={nextStep}
+          className="bg-green-500 text-white px-4 py-2 rounded"
+        >
+          Next
+        </button>
       </div>
 
-      <div className="mb-4">
-        <label className="block text-2xl font-semibold mb-1">City:</label>
-        <input
-          type="text"
-          name="city"
-          value={formData.city}
-          onChange={handleChange}
-          className="w-full border rounded p-2"
-          placeholder="Enter the city"
-          required
-        />
+      <div className="w-1/2 h-full bg-cover bg-center" style={{ backgroundImage: "url('/public/vk')" }}>
       </div>
-
-      <div className="mb-4">
-        <label className="block text-2xl font-semibold mb-1">Stadium:</label>
-        <input
-          type="text"
-          name="stadium"
-          value={formData.stadium}
-          onChange={handleChange}
-          className="w-full border rounded p-2"
-          placeholder="Enter the stadium"
-          required
-        />
-      </div>
-
-      <div className="mb-4">
-        <label className="block text-2xl font-semibold mb-1">Organizer Name:</label>
-        <input
-          type="text"
-          name="organizerName"
-          value={formData.organizerName}
-          onChange={handleChange}
-          className="w-full border rounded p-2"
-          placeholder="Enter the organizer's name"
-          required
-        />
-      </div>
-
-      <div className="mb-4">
-        <label className="block text-2xl font-semibold mb-1">Start Date:</label>
-        <input
-          type="date"
-          name="startDate"
-          value={formData.startDate}
-          onChange={handleChange}
-          className="w-full border rounded p-2"
-          required
-        />
-      </div>
-
-      <div className="mb-4">
-        <label className="block text-2xl font-semibold mb-1">End Date:</label>
-        <input
-          type="date"
-          name="endDate"
-          value={formData.endDate}
-          onChange={handleChange}
-          className="w-full border rounded p-2"
-          required
-        />
-      </div>
-
-      <button onClick={nextStep} className="bg-green-500 text-white px-4 py-2 rounded">
-        Next
-      </button>
     </div>
   );
 }
